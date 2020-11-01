989396116c9db7e41f45af6247df4c6a1e4f7a99
Evan Ristow <evan.ristow1@gmail.com>
2020-11-01 13:18:45 -0600

    more changes for lighthouse score of 100s...

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const Card = ({ id, name, email }) => {
+  const imgStyle = {
+    height: '200px',
+    width: '200px',
+  };
+
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
+      <img
+        src={`https://robohash.org/${id}?200x200`}
+        alt="robot"
+        style={imgStyle}
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,13 +3,15 @@ import React from 'react';
 const SearchBox = ({ searchField, searchChange }) => {
   return (
     <div className="pa2">
-      <input
-        className="pa3 ba b--green bg-lightest-blue"
-        type="search"
-        aria-label="Search Robots"
-        placeholder="Search Robots"
-        onChange={searchChange}
-      />
+      <label>
+        <input
+          className="pa3 ba b--green bg-lightest-blue"
+          type="search"
+          aria-label="Search Robots"
+          placeholder="Search Robots"
+          onChange={searchChange}
+        />
+      </label>
     </div>
   );
 };
