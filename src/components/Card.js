f8e8810748e2445038bcde49d3146a8b172bcf14
Evan Ristow <evan.ristow1@gmail.com>
2020-11-03 15:11:10 -0500

    Add new icons and update manifest

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ const Card = ({ id, name, email }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img
-        src={`https://robohash.org/${id}?200x200`}
+        src={`https://robohash.org/${id}?size=200x200`}
         alt="robot"
         style={imgStyle}
       />
